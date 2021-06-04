f68080a94afc1471fce071ba7d15cff93368c74f
Marie E. Rognes <meg@simula.no>
2021-06-04 11:23:19 +0200

    Update download link (updated dataset on Zenodo).

--- mri2fem/mri2fem/download_data.sh
+++ mri2fem/mri2fem/download_data.sh
@@ -1,6 +1,6 @@
 
 mkdir data
 cd data 
-wget https://zenodo.org/record/4386987/files/mri2fem-dataset.tar.gz?download=1  
-mv mri2fem-dataset.tar.gz?download=1  mri2fem-dataset.tar.gz 
+wget https://zenodo.org/record/4899120/files/mri2fem-dataset.tar.gz?download=1  
+mv mri2fem-dataset.tar.gz?download=1 mri2fem-dataset.tar.gz 
 tar xzvf mri2fem-dataset.tar.gz 
